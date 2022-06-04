240fa04f943e48a4e5303cfbf8d8993d6415f853
PCDotFan <i@g4.cx>
2022-06-04 22:45:52 +0800

    Initial commit

--- packages/renderer/react.config.ts
+++ packages/renderer/react.config.ts
@@ -23,13 +23,9 @@ export default defineConfig({
        *   'electron-store': 'const Store = require("electron-store"); export default Store;',
        * }
        */
-      
-      {
-        'electron-store': 'const Store = require("electron-store"); export default Store;',
-      }
     ),
   ],
-  base: process.env.GHPAGES==="TRUE" ?'/Vitron/':'./',
+  base: './',
   build: {
     sourcemap: true,
     outDir: '../../dist/renderer',
@@ -42,6 +38,7 @@ export default defineConfig({
   server: {
     host: pkg.env.VITE_DEV_SERVER_HOST,
     port: pkg.env.VITE_DEV_SERVER_PORT,
+    open: `http://${pkg.env.VITE_DEV_SERVER_HOST}:${pkg.env.VITE_DEV_SERVER_PORT}/`
   },
 })
 
--- packages/renderer/src/global.d.ts
+++ packages/renderer/src/global.d.ts
@@ -3,6 +3,9 @@ export { }
 
 declare global {
   interface Window {
+    // Expose some Api through preload script
+    fs: typeof import('fs')
+    ipcRenderer: import('electron').IpcRenderer
     removeLoading: () => void
   }
 }
