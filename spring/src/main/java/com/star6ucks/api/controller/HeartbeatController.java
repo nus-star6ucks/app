b33c4993866d01e1f8bbf79b42d77d1599185e3a
PCDotFan <i@g4.cx>
2022-06-05 16:07:28 +0800

    feat(misc): heartbeat implementation added & rename

--- spring/src/main/java/com/star6ucks/api/controller/HeartbeatController.java
+++ spring/src/main/java/com/star6ucks/api/controller/HeartbeatController.java
@@ -1,26 +1,18 @@
 package com.star6ucks.api.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.star6ucks.api.model.Item;
-import com.star6ucks.api.service.ItemService;
+import java.util.Collections;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api")
-public class ItemController {
-	@Autowired
-	private ItemService itemService;
-	
-	@GetMapping("/items")
-	public List<Item> getItems() {
-		return itemService.getItems(); 
-	}
-
+public class HeartbeatController {
+    @GetMapping("/heartbeats")
+    public Object heartbeats() {
+        return Collections.emptyList();
+    }
 }
--- spring/src/test/java/com/star6ucks/api/HeartbeatControllerAppIntegrationTest.java
+++ spring/src/test/java/com/star6ucks/api/HeartbeatControllerAppIntegrationTest.java
@@ -1,13 +1,17 @@
 package com.star6ucks.api;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HeartbeatControllerAppIntegrationTest {
@@ -16,7 +20,7 @@ public class HeartbeatControllerAppIntegrationTest {
 
     @Test
     public void whenTestAppThenEmptyResponse() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/heartbeat"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/heartbeats").accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json("{\"status\":1,\"errorCode\":\"\",\"errorMsg\":\"\",\"resultBody\":[]}"));
     }
