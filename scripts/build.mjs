84484bd50519f8a3f06f2a4fa9a3fb0d0a4ce23f
PCDotFan <i@g4.cx>
2022-06-05 16:30:40 +0800

    build(spring): auto jar packaging when vite build

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,5 +1,18 @@
-import { build } from 'vite'
-
-await build({ configFile: 'packages/main/vite.config.ts' })
-await build({ configFile: 'packages/preload/vite.config.ts' })
-await build({ configFile: 'packages/renderer/vite.config.ts' })
+import maven from 'maven';
+import { build } from 'vite';
+import { existsSync, mkdirSync, renameSync } from 'fs';
+import { join } from 'path';
+const mvn = maven.create({
+  cwd: 'spring',
+});
+await mvn.execute(['-B', 'package', '--file', 'pom.xml']);
+if (!existsSync(join(process.cwd(), 'libraries'))) {
+  mkdirSync(join(process.cwd(), 'libraries'));
+}
+renameSync(
+  join(process.cwd(), 'spring/target', 'spring.jar'),
+  join(process.cwd(), 'libraries', 'spring.jar'),
+);
+await build({ configFile: 'packages/main/vite.config.ts' });
+await build({ configFile: 'packages/preload/vite.config.ts' });
+await build({ configFile: 'packages/renderer/vite.config.ts' });
