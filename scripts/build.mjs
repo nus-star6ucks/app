63a1c3a057d3c83e50d7b2b9a29f4b3089d6ac7a
PCDotFan <i@g4.cx>
2022-06-07 11:20:07 +0800

    Release 0.0.4

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -4,10 +4,6 @@ import { existsSync, mkdirSync, renameSync } from 'fs';
 import { join } from 'path';
 import { execSync } from 'child_process';
 
-/**
- * child_process.spawn works a bit different when using
- * Windows.
- */
 const isWin = /^win/.test(process.platform);
 
 execSync(isWin ? `.\\gradlew.bat bootJar` : './gradlew bootJar', {
