11ce925060d7a49f1bdea01daac4adf6b32883c3
PCDotFan <i@g4.cx>
2022-06-09 01:09:16 +0800

    feat(store): ipc vuex sync

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -8,11 +8,27 @@
 import Vue from 'vue'
 import useSWRV from 'swrv'
 import { getHeartbeatService } from './services/heartbeat'
+import Store from 'electron-store'
+import { store } from "./store/index";
+
+const connectElectronStoreToVuex = () => {
+    const electronStore = new Store({ watch: true });
+    Object.keys(store.state).forEach((key) => {
+        console.log("register -> ", key);
+        electronStore.onDidChange(key, (value: any) => {
+            store.commit("updateByElectronStore", { key, value });
+        });
+    });
+};
+
 export default Vue.extend({
   setup() {
     useSWRV('/heartbeats', getHeartbeatService, {
       refreshInterval: 3000,
     })
   },
+  mounted() {
+    connectElectronStoreToVuex()
+  }
 })
 </script>
--- packages/renderer/src/store/index.ts
+++ packages/renderer/src/store/index.ts
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import createSharedMutations from '../utils/shared-mutations'
+import Store from 'electron-store'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  plugins: [createSharedMutations()],
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+const electronStore = new Store({ watch: true })
+
+export const store = new Vuex.Store({
+  state: {
+    text: '',
+  },
+  mutations: {
+    updateByElectronStore(state, payload: { key: string; value: any }) {
+      const curr = electronStore.get(payload.key)
+      if (curr && curr == state[payload.key as keyof typeof state]) return
+      state[payload.key as keyof typeof state] = payload.value
+      electronStore.set(payload.key, payload.value)
+    },
+  },
 })
