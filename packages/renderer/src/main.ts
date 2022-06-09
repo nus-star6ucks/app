0592cd9615651a66740acdf28c1f1c16edada66b
PCDotFan <i@g4.cx>
2022-06-10 00:11:01 +0800

    Release: 0.0.5

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -19,7 +19,6 @@ new Vue({
 
 if (window.removeLoading) window.removeLoading();
 
-// Usage of ipcRenderer.on
 if (window.ipcRenderer)
     window.ipcRenderer.on("main-process-message", (_event, ...args) => {
         console.log("[Receive Main-process message]:", ...args);
