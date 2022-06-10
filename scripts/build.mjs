f717d65a6dae4aafe38dfc04804443ba758849f0
PCDotFan <i@g4.cx>
2022-06-11 00:07:41 +0800

    Release: 0.0.5

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -5,19 +5,23 @@ import { execSync } from "child_process";
 
 const isWin = /^win/.test(process.platform);
 
-execSync(isWin ? `.\\gradlew.bat bootJar` : "./gradlew bootJar", {
-    cwd: "api",
+console.log('[BUILD] Executing gradle bootjar');
+
+execSync(isWin ? `.\\gradlew.bat bootJar` : './gradlew bootJar', {
+  cwd: 'api',
 });
 
-if (!existsSync(join(process.cwd(), "libraries"))) {
-    mkdirSync(join(process.cwd(), "libraries"));
+console.log('[BUILD] Move and rename api.jar file');
+
+if (!existsSync(join(process.cwd(), 'libraries'))) {
+  mkdirSync(join(process.cwd(), 'libraries'));
 }
-renameSync(
-    join(process.cwd(), "api/build/libs", "api.jar"),
-    join(process.cwd(), "libraries", "api.jar")
-);
+renameSync(join(process.cwd(), 'api/build/libs', 'api.jar'), join(process.cwd(), 'libraries', 'api.jar'));
+
+console.log('[BUILD] Vite building');
+
 (async () => {
-    await build({ configFile: "packages/main/vite.config.ts" });
-    await build({ configFile: "packages/preload/vite.config.ts" });
-    await build({ configFile: "packages/renderer/vite.config.ts" });
+  await build({ configFile: 'packages/main/vite.config.ts' });
+  await build({ configFile: 'packages/preload/vite.config.ts' });
+  await build({ configFile: 'packages/renderer/vite.config.ts' });
 })();
