c306d8be2faa4bb8316d0e758a4c2a9f9f0e681b
PCDotFan <i@g4.cx>
2022-06-11 21:17:36 +0800

    Release: 0.0.5

--- packages/renderer/src/utils.ts
+++ packages/renderer/src/utils.ts
@@ -1,4 +1,5 @@
 export const AVAILABLE_NOMIALS = [5, 10, 20, 50, 100];
+
 export const formatCentsText = (cents: number | string, convertToDollar = true) => {
   if (Number.isNaN(+cents)) return 'INVALID';
   if (cents >= 100 && convertToDollar) return `$${(+cents / 100).toFixed(2)}`;
