a649e48dad6d3a20cb5f758c0711d964709b0769
PCDotFan <i@g4.cx>
2022-06-11 15:55:08 +0800

    cleanup

--- packages/renderer/src/utils.ts
+++
@@ -1,6 +0,0 @@
-export const AVAILABLE_NOMIALS = [5, 10, 20, 50, 100];
-export const formatCentsText = (cents: number | string, convertToDollar = true) => {
-  if (Number.isNaN(+cents)) return 'INVALID';
-  if (cents >= 100 && convertToDollar) return `$${(+cents / 100).toFixed(2)}`;
-  return `${cents}C`;
-};
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ['./packages/renderer/index.html', './packages/renderer/src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    fontFamily: {
-      display: ['IBM Plex Mono', 'Menlo', 'monospace'],
-      body: ['IBM Plex Mono', 'Menlo', 'monospace'],
-    },
-  },
-}
