3aaee77e9cdf7435166741f0b7376f9f7bfaf93f
PCDotFan <i@g4.cx>
2022-06-20 23:46:35 +0800

    mock -> api

--- packages/renderer/src/utils.ts
+++ packages/renderer/src/utils.ts
@@ -1,6 +1,7 @@
 import { CoinApi, DrinkApi, MachineApi, UserApi } from './openapi'
 
-const baseUrl = 'https://mock.apifox.cn/m1/1108102-0-default'
+const baseUrl = 'http://localhost:8081'
+// const baseUrl = 'https://mock.apifox.cn/m1/1108102-0-default'
 
 export const coinApi = new CoinApi({}, baseUrl)
 export const machineApi = new MachineApi({}, baseUrl)
