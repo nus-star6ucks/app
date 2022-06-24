8d0399bb3981819c9a9b9580b8275fa45a348da8
ShiyuW <siyu115@gmail.com>
2022-06-24 17:16:06 +0800

    change annotation

--- api/src/test/java/com/mtech/vmcs/service/impl/CoinServiceImplTest.java
+++ api/src/test/java/com/mtech/vmcs/service/impl/CoinServiceImplTest.java
@@ -3,18 +3,19 @@ package com.mtech.vmcs.service.impl;
 import com.mtech.vmcs.model.entity.Coin;
 import com.mtech.vmcs.repository.CoinRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class CoinServiceImplTest {
 
     @Mock
@@ -25,7 +26,7 @@ class CoinServiceImplTest {
 
     @Test
     public void testGetCoins() {
-        List<Coin> coins = Arrays.asList(new Coin(123L, "Fanta", 85, 20, 1F));
+        List<Coin> coins = Collections.singletonList(new Coin(123L, "Fanta", 85, 20, 1F));
 
         when(coinRepository.findAll()).thenReturn(coins);
 
