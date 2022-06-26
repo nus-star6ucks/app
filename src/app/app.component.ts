0036aafbfaeca91d00834b7e251f52d2b40e2375
PCDotFan <i@g4.cx>
2022-06-27 01:11:06 +0800

    feat(HealthValidation): spring polling HealthValidation added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  BehaviorSubject,
+  concatMap,
+  from,
+  interval,
+  map,
+  Observable,
+  Subject,
+  Subscription,
+  switchMap,
+  takeWhile,
+  timer,
+} from 'rxjs';
 import { ElectronService } from './core/services';
 import { DataService } from './data.service';
-import { MachineService } from './http';
+import { DefaultService } from './http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private readonly electronService: ElectronService,
+    private readonly defaultService: DefaultService,
     private readonly dataService: DataService
   ) {
     if (electronService.isElectron) {
@@ -37,5 +51,24 @@ export class AppComponent {
     this.electronService.ipcRenderer.on('refresh-coin-states', () => {
       dataService.loadCoins();
     });
+  }
+
+  apiReady = false;
+  healthValidationSubscription: Subscription;
+  healthValidationSubject$ = new BehaviorSubject('');
+
+  ngOnInit(): void {
+    this.healthValidationSubscription = this.healthValidationSubject$
+      .pipe(
+        switchMap(_ =>
+          timer(0, 1000).pipe(
+            concatMap(_ => this.defaultService.actuatorHealthGet())
+          )
+        )
+      )
+      .subscribe(() => {
+        this.apiReady = true;
+        this.healthValidationSubscription.unsubscribe();
+      });
   }
 }
