876b8a77db8c8693e066b94243e8ce29fb416641
PCDotFan <i@g4.cx>
2022-06-29 17:03:18 +0800

    cleanup

--- api/src/main/java/com/mtech/vmcs/model/entity/InitialFileData.java
+++ api/src/main/java/com/mtech/vmcs/model/entity/InitialFileData.java
@@ -4,20 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
 
+@Getter
+@Setter
 public class InitialFileData {
-  @Getter
-  @Setter
+
   private List<User> users;
 
-  @Getter
-  @Setter
   private List<Machine> machines;
 
-  @Getter
-  @Setter
   private List<Drink> drinks;
 
-  @Getter
-  @Setter
   private List<Coin> coins;
 }
